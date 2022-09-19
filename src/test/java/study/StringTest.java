ed2bca961b7af14edb7931378a418b1bb7648781
kjuiop <copying_y@naver.com>
2022-09-20 06:43:00 +0900

    [StringClass] 요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,16 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("1,2 문자열을 , 기준으로 Split 테스트")
+    void splitTest() {
+        String actual = "1,2";
+        String[] expected = {"1", "2"};
+        String[] result = actual.split(",");
+
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly(expected);
     }
 }
