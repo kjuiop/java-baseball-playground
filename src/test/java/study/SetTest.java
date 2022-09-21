1dae0f138b27a006a00389a3ddb67edeb1f47291
kjuiop <copying_y@naver.com>
2022-09-21 15:20:40 +0900

    [SetClass] 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,8 +30,15 @@ public class SetTest {
 
     @Test
     @DisplayName("Set 의 size() 메소드를 활용해 Set 의 크기를 확인하는 테스트")
-    void replace() {
+    void checkSetCollectionSize() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("컬렉션의 요소값을 포함하는지에 대한 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int element) {
+        assertThat(numbers.contains(element)).isTrue();
     }
 
 }
