5fc585da71cc12d36d965c2e9871125cc9cbf920
kjuiop <copying_y@naver.com>
2022-09-21 15:09:41 +0900

    [StringClass] 요구사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -31,5 +32,17 @@ public class StringTest {
         String result = actual.substring(1, 4);
 
         assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져올 때 위치값을 벗어나면 에러발생")
+    void charAtTest() {
+        String actual = "abc";
+        int index = 5;
+
+        assertThatThrownBy(() -> actual.charAt(index))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range")
+                .hasMessageContaining(String.valueOf(index));
     }
 }
